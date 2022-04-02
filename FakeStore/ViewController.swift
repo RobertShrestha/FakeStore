24d25ea9e93b005ca7e6beeb11f613622cd1085d
Robert Shrestha <robertshrestha2068@gmail.com>
2022-04-02 22:30:32 +0545

    Initial commit

---
+++ FakeStore/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FakeStore
+//
+//  Created by Robert Shrestha on 4/2/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print(Environment.rootURL.absoluteString)
+    }
+
+
+}
+
