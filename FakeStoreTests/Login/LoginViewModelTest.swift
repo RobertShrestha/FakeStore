2b4848a7d7de4941d33bf0e5053ca47ea6e553aa
Robert Shrestha <robertshrestha2068@gmail.com>
2022-04-04 22:10:02 +0545

    .gitignore added

--- FakeStoreTests/Login/LoginViewModelTest.swift
+++ FakeStoreTests/Login/LoginViewModelTest.swift
@@ -6,8 +6,101 @@
 //
 
 import XCTest
-
+import RxTest
+import RxBlocking
+import RxSwift
+import RxCocoa
+@testable import FakeStore
 class LoginViewModelTest: XCTestCase {
+
+    var disposeBag: DisposeBag!
+    var viewModel: LoginViewModel!
+    var client: MockLoginService!
+    var scheduler: ConcurrentDispatchQueueScheduler!
+    var testScheduler: TestScheduler!
+
+    override func setUp() {
+        super.setUp()
+        disposeBag = DisposeBag()
+        client = MockLoginService(client: WebServices.shared)
+        viewModel = LoginViewModel(withLogin: client)
+        scheduler = ConcurrentDispatchQueueScheduler(qos: .default)
+        testScheduler = TestScheduler(initialClock: 0)
+    }
+    override func tearDown() {
+        disposeBag = nil
+        viewModel = nil
+        scheduler = nil
+        testScheduler = nil
+        super.tearDown()
+    }
+    /// Check if the isvalid is true if all the fields are correct
+    func testLoginViewModel_WhenInformationProvidedisInvalid_IsValidShouldBeFalse() {
+        let isValid = viewModel.isValid.subscribe(on: scheduler)
+        viewModel.username.accept("")
+        viewModel.password.accept("")
+        XCTAssertEqual(try isValid.toBlocking().first(), false)
+    }
+    func testLoginViewModel_WhenInformationProvidedisValid_IsValidShouldBeTrue() {
+        let isValid = viewModel.isValid.subscribe(on: scheduler)
+        viewModel.username.accept("ro")
+        viewModel.password.accept("ro")
+        XCTAssertEqual(try isValid.toBlocking().first(), true)
+    }
+    func testLoginService_WhenInformationProvidedisValid_LoginIsSuccess() {
+
+        viewModel.username.accept("ro")
+        viewModel.password.accept("ro")
+        guard let url = URLBuilder()
+                .setPath(paths: .login)
+                .build() else { return }
+        let request = RequestLoginModel(username: self.viewModel.username.value, password: self.viewModel.password.value)
+
+        var resource = Resource<LoginModel>(url: url)
+        resource.body =  request.toData()
+        resource.httpMethod = .post
+        let response = self.client.doLogin(resource: resource)
+        _  = response.subscribe(onSuccess: { model in
+            guard let token = model.token else {
+                XCTFail("The token cannot be empty")
+                return
+            }
+            XCTAssertTrue(!token.isEmpty)
+        }, onFailure: { error in
+            print(error)
+        })
+    }
+
+    func testLoginService_WhenInformationProvidedisInValid_LoginIsFailure() {
+
+        viewModel.username.accept("")
+        viewModel.password.accept("")
+        guard let url = URLBuilder()
+                .setPath(paths: .login)
+                .build() else { return }
+        let request = RequestLoginModel(username: self.viewModel.username.value, password: self.viewModel.password.value)
+
+        var resource = Resource<LoginModel>(url: url)
+        resource.body =  request.toData()
+        resource.httpMethod = .post
+        let response = self.client.doLogin(resource: resource)
+        _  = response.subscribe(onSuccess: { model in
+            XCTFail("This should not be here")
+        }, onFailure: { error in
+            print(error)
+            let error = error as? APIErrors
+            switch error {
+            case .seralizationError:
+                XCTFail("This should not be here")
+            case .apiError(let message):
+                XCTAssertEqual(message, "Something went wrong")
+            case .domainError(_):
+                XCTFail("This should not be here")
+            case .none:
+                XCTFail("This should not be here")
+            }
+        })
+    }
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
---
+++ Podfile
@@ -0,0 +1,25 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '11.0'
+
+target 'FakeStore' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+    pod 'RxSwift'
+    pod 'RxCocoa'
+    pod 'Kingfisher', '~> 5.0'
+    pod 'IQKeyboardManagerSwift', '~> 6.5.0'
+    pod 'MBProgressHUD'
+    pod 'RxAlamofire'
+    pod 'SwiftyBeaver'
+    #pod 'SwiftLint'
+  # Pods for NewsApp
+
+  target 'FakeStoreTests' do
+    inherit! :search_paths
+    # Pods for testing
+    pod 'RxTest'
+    pod 'RxBlocking'
+  end
+
+end
